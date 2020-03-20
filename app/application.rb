6bf16277db42f513cef03ae2ed450a5c69323d2a
Kyle Talmage <ktalmage85@gmail.com>
2020-03-20 16:00:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,15 @@
-class MyServer
-  def call(env)
-    return [ 200, {'Content-Type' => 'text/html'}, time ]
-  end
- 
-  def time
-    if (Time.now.to_i) > 12   
-      ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
+class Application
+def call(env)
+    resp = Rack::Response.new
+
+    time = Time.now.hour
+
+    if time <= 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
   end
 end
